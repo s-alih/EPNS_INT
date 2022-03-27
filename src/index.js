2366318d250d1ec79f2603555178d3a273707b05
s-alih <slh32064@gmail.com>
2022-03-27 09:31:25 +0530

    ok

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
 const express = require('express'); 
+const hbs = require('hbs');
+const async = require('hbs/lib/async');
+const path = require('path')
+
 const app = express();   
 const connection = require('./connections/database')
 require('../src/connections/database')
@@ -6,7 +10,12 @@ require('dotenv').config();
 const fetchAllEvents = require('./utils/eventHandlers')
 
 
+const viewsPath = path.join(__dirname,'../templets/views')
+const partialsPath = path.join(__dirname,'../templets/partials')
 
+app.set('view engine','hbs')
+app.set('views',viewsPath)
+hbs.registerPartials(partialsPath)
 
 const port = 5000; 
 
@@ -14,14 +23,19 @@ const port = 5000;
 
 
 app.get('/',async (req, res) => { 
-    res.send('start',);                                                   
+    res.render('index',);                                                   
 });
 
 
-app.get('/fetchEvents',async (req,res)=>{
-    const events = await fetchAllEvents()
-    console.log(events)
-    res.send(events)
+// app.get('/fetchAndStoreEvents',async (req,res)=>{
+//     const events = await fetchAllEvents()
+//     console.log(events)
+//     res.send(events)
+// })
+
+
+app.get("/invoke",async(req,res)=>{
+
 })
 
 
@@ -42,25 +56,17 @@ app.get('/createEventTable',async(req,res)=>{
 
 
 
-
-
-
-app.get('/pushToDb',async(req,res)=>{
-
+app.get('/fetchAndStoreEvents',async(req,res)=>{
     const events = await fetchAllEvents()
-   
-    let query = `INSERT INTO Events(blockHash, event) VALUES`;
+    let query = `INSERT IGNORE INTO Events(blockHash, event) VALUES`;
     events.forEach((e)=>{
-    
         query += `('${e.blockHash}','${JSON.stringify(e)}'),`
-
     })
-
     connection.query(`${query.slice(0,-1)}`,(err, results, fields) => {
         if (err) {
             return res.send(err.message)
         }else{
-            res.send(results)
+            res.send(events)
         }
         
     })
