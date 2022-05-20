df64b24ed1c8b21ddcbaa5b6a72f7c5f8ea9d370
s-alih <slh32064@gmail.com>
2022-05-20 09:06:34 +0530

    tested

--- src/utils/eventHandlers.js
+++ src/utils/eventHandlers.js
@@ -37,6 +37,24 @@ const fetchTokenBalance = async()=>{
     const builder = AhoCorasick.builder();
     wallets.forEach(k => builder.add(k));
     const ac = builder.build(); // This is expensive (!)
+    const prevBlockNumber  = 31684742;
+    // get current block number
+    const currentBlockNumber = await provider.getBlockNumber();
+    console.log(currentBlockNumber)
+
+    for(let i =prevBlockNumber; i<= currentBlockNumber;i++ ){
+        console.log(i)
+        const data = await provider.getBlockWithTransactions(i)
+        data.transactions.forEach((transaction)=>{
+            const list = ac.match(JSON.stringify(transaction))
+            if(list.length !== 0){
+                console.log(list) ;
+            }
+        })
+
+    }
+
+    
     
     // now it's time to cache ac in between queries in a shared memory
     // and refresh it when any subsriber is added/or removed
@@ -44,57 +62,6 @@ const fetchTokenBalance = async()=>{
     
     // on each block
     
-    provider.on("block",async (blockNumber) => {
-        console.log(blockNumber)
-      // get block
-     const data = await provider.getBlockWithTransactions(blockNumber)
-    
-    // {
-    //   _difficulty: { BigNumber: "3849295379889" },
-    //   difficulty: 3849295379889,
-    //   extraData: '0x476574682f76312e302e312d39383130306634372f6c696e75782f676f312e34',
-    //   gasLimit: { BigNumber: "3141592" },
-    //   gasUsed: { BigNumber: "21000" },
-    //   hash: '0xf93283571ae16dcecbe1816adc126954a739350cd1523a1559eabeae155fbb63',
-    //   miner: '0x909755D480A27911cB7EeeB5edB918fae50883c0',
-    //   nonce: '0x1a455280001cc3f8',
-    //   number: 100004,
-    //   parentHash: '0x73d88d376f6b4d232d70dc950d9515fad3b5aa241937e362fdbfd74d1c901781',
-    //   timestamp: 1439799168,
-    //   transactions: [
-    //     {
-    //       accessList: null,
-    //       blockHash: '0xf93283571ae16dcecbe1816adc126954a739350cd1523a1559eabeae155fbb63',
-    //       blockNumber: 100004,
-    //       chainId: 0,
-    //       confirmations: 14581276,
-    //       creates: null,
-    //       data: '0x',
-    //       from: '0xcf00A85f3826941e7A25BFcF9Aac575d40410852',
-    //       gasLimit: { BigNumber: "90000" },
-    //       gasPrice: { BigNumber: "54588778004" },
-    //       hash: '0x6f12399cc2cb42bed5b267899b08a847552e8c42a64f5eb128c1bcbd1974fb0c',
-    //       nonce: 25,
-    //       r: '0xb23adc880d3735e4389698dddc953fb02f1fa9b57e84d3510a2a4b3597ac2486',
-    //       s: '0x4e856f95c4e2828933246fb4765a5bfd2ca5959840643bef0e80b4e3a243d064',
-    //       to: '0xD9666150A9dA92d9108198a4072970805a8B3428',
-    //       transactionIndex: 0,
-    //       type: 0,
-    //       v: 27,
-    //       value: { BigNumber: "5000000000000000000" },
-    //       wait: [Function]
-    //     }
-    //   ]
-    // }
-    data.transactions.forEach((transaction)=>{
-        const list = ac.match(JSON.stringify(transaction))
-        if(list.length !== 0){
-            console.log(list) ;
-        }
-    })
-    
-    
-    })
 
 }
 
